d51d2e1565f43e0c7a4aa2e31b906346eb3c8db2
Zergi <bestestyasuo@gmail.com>
2025-02-04 18:32:31 +0100

    added base

---
+++ chessApp/Pieces/Location.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chessApp.Pieces
+{
+    public class Location
+    {
+        public Location(string x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public string X { get; set; }
+        public int Y { get; set; }
+    }
+}
---
+++ chessApp/Pieces/Piece.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chessApp.Pieces
+{
+    abstract class Piece
+    {
+        abstract public List<Location> CanMoveTo();
+        abstract public void MoveTo(Location LocationToMove);
+
+        public void GetTaken()
+        {
+            Alive = false;
+        }
+
+        private String Color;
+        private Location Location;
+        private Boolean Alive = true;
+
+    }
+}

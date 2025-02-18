2925a7c8cec2e72958774d3db6496f4b41fd8f7f
Zergi <bestestyasuo@gmail.com>
2025-02-18 15:01:24 +0100

    added board and game

---
+++ chessApp/Game/Game.cs
@@ -0,0 +1,16 @@
+﻿using chessApp.Board;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chessApp.Game
+{
+    public class Game : IGame
+    {
+        public IBoard board {  get; set; }
+
+       
+    }
+}

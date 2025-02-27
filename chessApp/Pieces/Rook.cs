a745d7410d25056f2e1cbb6ff9af51f84762cc6f
Zergi <bestestyasuo@gmail.com>
2025-02-27 20:19:33 +0100

    rook started

--- chessApp/Pieces/Rook.cs
+++ chessApp/Pieces/Rook.cs
@@ -1,9 +1,28 @@
-﻿namespace chessApp.Pieces
+﻿
+namespace chessApp.Pieces
 {
     public class Rook : BasePiece
     {
         public Rook(Colour colour, Location location) : base(colour, location)
         {
+        }
+
+        public override void MoveTo(Location to, List<BasePiece> pieces)
+        {
+            //can only move up or down or sideways, any number of squares but not both
+            if((Location.X == to.X && Location.Y == to.Y) || (Location.X != to.X && Location.Y != to.Y))
+            {
+                throw new Exception();
+            }
+            if (Location.X == to.X && Location.Y != to.Y)
+            {
+                var difference = Location.Y - to.Y; 
+            }
+            if (Location.X != to.X && Location.Y == to.Y)
+            {
+
+            }
+
         }
     }
 }

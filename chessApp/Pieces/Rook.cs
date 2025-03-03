259944eec52abf87e1256cd1e7fa9aa6c157144d
Zergi <bestestyasuo@gmail.com>
2025-03-03 13:43:52 +0100

    added rook moveto

--- chessApp/Pieces/Rook.cs
+++ chessApp/Pieces/Rook.cs
@@ -10,19 +10,29 @@ namespace chessApp.Pieces
         public override void MoveTo(Location to, List<BasePiece> pieces)
         {
             //can only move up or down or sideways, any number of squares but not both
-            if((Location.X == to.X && Location.Y == to.Y) || (Location.X != to.X && Location.Y != to.Y))
+            if ((Location.X == to.X && Location.Y == to.Y) || (Location.X != to.X && Location.Y != to.Y))
             {
                 throw new Exception();
             }
-            if (Location.X == to.X && Location.Y != to.Y)
+            else if (pieces.Any(p =>
+                (Location.X == to.X && p.Location.X == Location.X &&
+                 p.Location.Y > Math.Min(Location.Y, to.Y) &&
+                 p.Location.Y < Math.Max(Location.Y, to.Y)) ||
+                (Location.Y == to.Y && p.Location.Y == Location.Y &&
+                 p.Location.X > Math.Min(Location.X, to.X) &&
+                 p.Location.X < Math.Max(Location.X, to.X))))
             {
-                var difference = Location.Y - to.Y; 
+                throw new Exception();
             }
-            if (Location.X != to.X && Location.Y == to.Y)
+            else
             {
-
+                var piece = pieces.FirstOrDefault(p => p.Location.X == to.X && p.Location.Y == to.Y && p.Colour != Colour);
+                if (piece != null)
+                {
+                    pieces.Remove(piece);
+                }
+                Location = to;
             }
-
         }
     }
 }

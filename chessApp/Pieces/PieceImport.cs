393b5fc667c95ab7a5dcef36fbc45570616e2665
Zergi <bestestyasuo@gmail.com>
2025-03-07 16:36:04 +0100

    removed required from PieceImport, added constructor, added xUnit

--- chessApp/Pieces/PieceImport.cs
+++ chessApp/Pieces/PieceImport.cs
@@ -3,7 +3,14 @@
     public class PieceImport
     {
         public Colour Colour { get; set; }
-        public required Location Location { get; set; }
-        public required string Type { get; set; }
+        public Location Location { get; set; }
+        public  string Type { get; set; }
+
+        public PieceImport(Colour colour, Location location, string type)
+        {
+            Colour = colour;
+            Location = location;
+            Type = type;
+        }
     }
 }

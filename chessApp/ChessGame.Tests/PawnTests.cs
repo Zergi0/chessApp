1f51d8f5ab7a019cdd8f89ad0ac8b7d23d98a825
Zergi <bestestyasuo@gmail.com>
2025-03-11 14:26:40 +0100

    updated pawn tests with Parameterized tests

--- chessApp/ChessGame.Tests/PawnTests.cs
+++ chessApp/ChessGame.Tests/PawnTests.cs
@@ -17,55 +17,23 @@ public class PawnTests : IDisposable
         chessService.EmptyTheBoard();
     }
 
-    [Fact]
-    public void TestPawnMove1StepFromSpawnWhite()
+    [Theory]
+    [InlineData('A',3,Colour.white,'A',2)]
+    [InlineData('A',4,Colour.white,'A',2)]
+    [InlineData('A',6,Colour.black,'A',7)]
+    [InlineData('A',5,Colour.black,'A',7)]
+    [InlineData('A',4,Colour.white,'A',3)]
+    [InlineData('A',4,Colour.black,'A',5)]
+    public void MovePawnForward(char toX, int toY,Colour colour,char fromX, int fromY)
     {
-        List<PieceImport> pieces = new() { new(Colour.white, new Location('A', 2), "pawn") };
+        List<PieceImport> pieces = new() { new(colour, new Location(fromX,fromY), "pawn") };
         chessService.CreateCustomBoard(pieces);
 
-        Location to = new Location('A', 3);
+
+        Location to = new Location(toX,toY);
         chessService.MovePiece(pieces[0].Location, to);
 
-        Location expected = new Location('A', 3);
-        Assert.Equal(expected.ToString(), chessService.Pieces[0].Location.ToString());
-    }
-
-    [Fact]
-    public void TestPawnMove2StepFromSpawnWhite()
-    {
-        List<PieceImport> pieces = new() { new(Colour.white, new Location('A', 2), "pawn") };
-        chessService.CreateCustomBoard(pieces);
-
-        Location to= new Location('A', 4);
-        chessService.MovePiece(pieces[0].Location, to);
-
-        Location expected = new Location('A', 4);
-        Assert.Equal(expected.ToString(), chessService.Pieces[0].Location.ToString());
-    }
-
-    [Fact]
-    public void TestPawnMove1StepFromSpawnBlack()
-    {
-        List<PieceImport> pieces = new() { new(Colour.black, new Location('A', 7), "pawn") };
-        chessService.CreateCustomBoard(pieces);
-
-        Location to = new Location('A', 6);
-        chessService.MovePiece(pieces[0].Location, to);
-
-        Location expected = new Location('A', 6);
-        Assert.Equal(expected.ToString(), chessService.Pieces[0].Location.ToString());
-    }
-
-    [Fact]
-    public void TestPawnMove2StepFromSpawnBlack()
-    {
-        List<PieceImport> pieces = new() { new(Colour.black, new Location('A', 7), "pawn") };
-        chessService.CreateCustomBoard(pieces);
-
-        Location to = new Location('A', 5);
-        chessService.MovePiece(pieces[0].Location, to);
-
-        Location expected = new Location('A', 5);
+        Location expected = new Location(toX, toY);
         Assert.Equal(expected.ToString(), chessService.Pieces[0].Location.ToString());
     }
 
@@ -127,37 +95,35 @@ public class PawnTests : IDisposable
         Assert.Equal($"Cannot make move to {to.X}{to.Y} from {pieces[0].Location.X}{pieces[0].Location.Y}.", exception.Message);
     }
 
-    [Fact]
-    public void TestPawnTakeMoveOpponentPieceRightDirection()
-    {
-        List<PieceImport> pieces = new() { new(Colour.white, new Location('A', 2), "pawn"), new(Colour.black, new Location('B', 3), "pawn") };
-        Location to = new Location('B', 3);
-        chessService.CreateCustomBoard(pieces);
 
+    [Theory]
+    [InlineData('C',3,Colour.white,'B',2,Colour.black)]
+    [InlineData('A',3,Colour.white,'B',2,Colour.black)]
+    [InlineData('A',6,Colour.black,'B',7,Colour.white)]
+    [InlineData('C',6,Colour.black,'B',7,Colour.white)]
+    public void MovePawnToTake(char toX, int toY, Colour colourSelf, char fromX, int fromY,Colour colourOpponent)
+    {
+        List<PieceImport> pieces = new() { new(colourSelf, new Location(fromX,fromY), "pawn"), new(colourOpponent, new Location(toX,toY), "pawn") };
+        Location to = new Location(toX,toY);
+        chessService.CreateCustomBoard(pieces);
 
         chessService.MovePiece(pieces[0].Location, to);
         int expectedAmount = 1;
-        Location expected = new Location('B', 3);
+        Location expected = new Location(toX, toY);
+
 
         Assert.Equal(expected.ToString(), chessService.Pieces[0].Location.ToString());
-        Assert.Equal(pieces.Count,expectedAmount);
+        Assert.Equal(chessService.Pieces.Count, expectedAmount);
     }
-
-
     [Fact]
-    public void TestPawnTakeMoveOpponentPieceLeftDirection()
+    public void MovePawnToTakeWrongDirection_ShouldThrowInvalidOperationException()
     {
-        List<PieceImport> pieces = new() { new(Colour.white, new Location('B', 2), "pawn"), new(Colour.black, new Location('A', 3), "pawn") };
-        Location to = new Location('A', 3);
+        List<PieceImport> pieces = new() { new(Colour.white, new Location('A', 3), "pawn"), new(Colour.black, new Location('B', 2), "pawn") };
+        Location to = new Location('B', 2);
         chessService.CreateCustomBoard(pieces);
 
+        var exception = Assert.Throws<InvalidOperationException>(() => chessService.MovePiece(pieces[0].Location, to));
 
-        chessService.MovePiece(pieces[0].Location, to);
-        int expectedAmount = 1;
-        Location expected = new Location('A', 3);
-
-        Assert.Equal(expected.ToString(), chessService.Pieces[0].Location.ToString());
-        Assert.Equal(pieces.Count, expectedAmount);
+        Assert.Equal($"Cannot make move to {to.X}{to.Y} from {pieces[0].Location.X}{pieces[0].Location.Y}.", exception.Message);
     }
-
 }
